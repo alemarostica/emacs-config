d967b5d9b3a411074e5008de25bb92d723475f2b
alemarostica <me@alessandromarostica.com>
2025-09-15 11:20:10 +0200

    Added md mode

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -9,13 +9,7 @@
      default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(abyss-theme adwaita-dark-theme adwaita-dark-theme-theme
-		 atom-one-dark-theme auto-dark-theme company
-		 dash-functional github-dark-vscode-theme
-		 gruber-darker-theme gruvbox-theme magit
-		 monokai-alt-theme rust-mode smex vs-dark-theme
-		 vscode-dark-plus-theme vscode-dark-plus-theme-theme
-		 vscode-dark-theme)))
+   '(company dash-functional gruvbox-theme magit markdown-mode smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -28,6 +28,9 @@
 (use-package magit
   :ensure t)
 
+(use-package markdown-mode
+  :ensure t)
+
 (use-package company
   :ensure t)
 
