3b5bd2d57d38e6ec0d7f178d1e63062015ee2655
alemarostica <me@alessandromarostica.com>
2025-12-24 14:10:04 +0100

    themes messing

--- early-init.el
+++ early-init.el
@@ -1,2 +1,2 @@
-(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font Mono-13"))
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-11"))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
